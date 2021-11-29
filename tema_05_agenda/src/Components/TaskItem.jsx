b151a0bb7109013db1109a70e3363a82fc49b5f7
Rafael Lopes dos Santos <rr.lores@gmail.com>
2021-11-29 19:02:46 -0300

    update final

--- tema_05_agenda/src/Components/TaskItem.jsx
+++ tema_05_agenda/src/Components/TaskItem.jsx
@@ -1,7 +1,18 @@
-const TaskItem = ({ task }) => {
+import { FaTimes } from 'react-icons/fa';
+
+const TaskItem = ({ task, onDelete, onToggle }) => {
     return (
-        <div className="task">
-            <h3>{task.text}</h3>
+        <div
+            className={`task ${task.reminder ? 'reminder' : ''}`}
+            onDoubleClick={() => onToggle(task.id)}
+        >
+            <h3>
+                {task.text}
+                <FaTimes
+                    style={{ color: 'red', cursor: 'pointer' }}
+                    onClick={() => onDelete(task.id)}
+                />
+            </h3>
             <p>{task.day}</p>
         </div>
     );
--- tema_05_agenda/src/Components/Tasks.jsx
+++ tema_05_agenda/src/Components/Tasks.jsx
@@ -1,10 +1,10 @@
 import TaskItem from './TaskItem';
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete }) => {
     return (
         <>
             {tasks.map((task) => (
-                <TaskItem key={task.id} task={task} />
+                <TaskItem key={task.id} task={task} onDelete={onDelete} />
             ))}
         </>
     );
