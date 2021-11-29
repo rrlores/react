8c5ac32f386ec1bf48c2b5334828be37228c19db
Rafael Lopes dos Santos <rr.lores@gmail.com>
2021-11-29 14:51:05 -0300

    update

---
+++ tema_05_agenda/src/Components/TaskItem.jsx
@@ -0,0 +1,10 @@
+const TaskItem = ({ task }) => {
+    return (
+        <div className="task">
+            <h3>{task.text}</h3>
+            <p>{task.day}</p>
+        </div>
+    );
+};
+
+export default TaskItem;
